c09ceac54a2752c139a652ca452e62ad07507ecd
Gabriel Noal <gabriel.oliva@hotmail.com>
2020-03-27 09:58:47 -0300

    fixes

--- aps.py
+++ aps.py
@@ -21,7 +21,7 @@ print("matriz dos nos: {}".format(matriz_dos_nos))
 # print("o numero de membros: {}".format(numero_de_membros))
 # print("matriz de incidencia: {}".format(matriz_de_incidencia))
 # print("numero de cargas: {}".format(numero_de_cargas))
-# print("vetor carregamento: {}".format(vetor_carregamento))
+print("vetor carregamento {}x1: {}".format(len(vetor_carregamento),vetor_carregamento))
 # print("numero de restricoes: {}".format(numero_de_restricoes))
 # print("vetor restricoes: {}".format(vetor_restricoes))
 
@@ -30,7 +30,6 @@ Matriz_base = [[0 for x in range(numero_i_j)] for y in range(numero_i_j)]
 elementos = []
 pontos = []
 vetorP = []
-vetorPCarregamento = []
 
 def main():
   for index_do_no in range(numero_de_nos):
@@ -39,25 +38,20 @@ def main():
     pontos.append(Ponto(numero_do_no, cordenadas))
 
 
-  for gdl_index in range(2*numero_de_membros):
+  vetorP_forcas = []
+  for gdl_index in range(2*numero_de_nos):
     gdl = gdl_index + 1
     if gdl_index in vetor_restricoes:
       vetorP.append('r')
-      vetorPCarregamento.append(0.0)
+      vetorP_forcas.append(0.0)
     else:
       vetorP.append(gdl)
-      vetorPCarregamento.append(vetor_carregamento[gdl_index])
+      vetorP_forcas.append(vetor_carregamento[gdl_index])
 
-  # print("vetorPCarregamento: {}".format(vetorPCarregamento))
 
-  vetorP_forcas = []    
-  for item in vetorP:
-      if(item != 'r'):
-        vetorP_forcas.append(vetor_carregamento[item - 1])
-      else:
-        vetorP_forcas.append(0.0)
-  # print("vetorP_forcas: {}".format(vetorP_forcas))
-        
+  # print("vetorP: {}".format(vetorP))
+  print("vetorP_forcas: {}".format(vetorP_forcas))
+
   for i in range(numero_de_membros):
     numero_do_elemento = i + 1
     incidencia = [int(matriz_de_incidencia[i][0]), int(matriz_de_incidencia[i][1])]
@@ -84,7 +78,7 @@ def main():
     matrizGlobal = retorna_matriz_global(elemento.gdls, elemento.matrizK, Matriz_base, numero_i_j)
 
   #contorno da matriz 
-  # print("Matriz Global {}x{}: {}".format(len(matrizGlobal), len(matrizGlobal[0]), matrizGlobal) + "\n")
+  print("Matriz Global {}x{}: {}".format(len(matrizGlobal), len(matrizGlobal[0]), matrizGlobal) + "\n")
   matrizContornada , vetorContornado, index_do_corte = AplicarContorno(matrizGlobal, vetorP, vetor_carregamento)
   # print("Matriz Contornada {}x{} : {}".format(len(matrizContornada),len(matrizContornada[0]),matrizContornada)  + "\n")
 
@@ -98,7 +92,7 @@ def main():
 
   vetor_deslcamento_completo = refazerContorno(vetor_deslocamento_contornado, index_do_corte, numero_de_nos)
   
-  print("Vetor deslocamento completo: {}".format(vetor_deslcamento_completo) + "\n")
+  print("Vetor deslocamento completo: {}x1 {}".format(len(vetor_deslcamento_completo),vetor_deslcamento_completo) + "\n")
   # print("Vetor deslocamento: {}".format(vetor_deslocamento_contornado) + "\n")
   ##deformação
   lista_ti = []
@@ -110,10 +104,10 @@ def main():
     sen = elemento.sen
     cos = elemento.cos
     l = elemento.L
-    matriz = [-cos, -sen, cos, -sen]
+    matriz = [-cos, -sen, cos, sen]
     vetor_def=[]
-    for index in elemento.gdls:
-      vetor_def.append(vetor_deslcamento_completo[index-1])
+    for gdl in elemento.gdls:
+      vetor_def.append(vetor_deslcamento_completo[gdl-1])
     epsi = (1/l) * np.dot(matriz,vetor_def)
     ti, fi = elemento.setForces(epsi)
     lista_ti.append(ti)
@@ -129,11 +123,12 @@ def main():
   #   [deslocamentoX, deslocamentoY] = vetor_deslcamento_completo[(matriz_index * 2): (matriz_index * 2 + 2)]
   #   nova_matriz_dos_nos[0][matriz_index] += deslocamentoX
   #   nova_matriz_dos_nos[1][matriz_index] += deslocamentoY
+  # dot =  np.array(matrizGlobal).dot(vetor_deslcamento_completo)
+  # print("Reações de apoio: {}".format(dot))
+  geraSaida("saida.txt", vetorP_forcas, vetor_deslcamento_completo, list_epsi, lista_fi, lista_ti)
   # plota(matriz_dos_nos, matriz_de_incidencia)
   # plota(nova_matriz_dos_nos, matriz_de_incidencia)
 
-
-  geraSaida("saida.txt", vetorPCarregamento, vetor_deslcamento_completo, list_epsi, lista_fi, lista_ti)
     
 
 main()
