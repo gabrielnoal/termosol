2a7c16caef42b50bc2bbef33a87d729ccc2e7ba9
Gabriel Noal <gabriel.oliva@hotmail.com>
2020-05-19 17:52:32 -0300

    add aps2.py

---
+++ APS2/aps2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from math import *
+import numpy as np
+
+
+x_min = 0.0
+x_max = 0.5
+y_min = 0.0
+y_max = 0.5
+t_max = 10.0
+delta_x = 0.1
+delta_y = delta_x
+delta_t = 10.0**(-2)
+
+alpha = 0.25
+f0 = alpha * (delta_t/((delta_x)**2))
+#                        top    rigth  bottom   left 
+temperaturas_iniciais = [100.0, 0.0, 0.0, 0.0]
+
+malha_x = [x for x in np.arange(x_min , x_max + delta_x, delta_x)]
+malha_y = [y for y in np.arange(y_min , y_max + delta_y, delta_y)]
+
+malha = np.zeros(shape=(len(malha_x), len(malha_y)))
+malha[0].fill(temperaturas_iniciais[0])
+malha[-1].fill(temperaturas_iniciais[-2])
+for i in np.arange(1, len(malha_x) - 1, 1):
+  malha[i][-1] = temperaturas_iniciais[1]
+  malha[i][0] = temperaturas_iniciais[-1]
+
+print("malha:\n {}\n".format(malha))
+malha_atualizada = np.copy(malha)
+
+
+for t in np.arange(0 , t_max + delta_t, delta_t):
+  for i in np.arange(1, len(malha_x) - 1, 1):
+    for j in np.arange(1, len(malha_y) - 1, 1):
+      malha_atualizada[i][j] = f0 * (malha[i + 1][j] + malha[i - 1][j] + malha[i][j + 1] + malha[i][j - 1]) + (1 - 4*f0)*malha[i][j]
+  malha_atualizada = np.copy(malha_atualizada)
+
+print("malha_atualizada:\n {}\n".format(malha_atualizada))
